55ecc53064252690698912dff0cb31ae557532f9
Bas Dijkstra <bas@ontestautomation.com>
2024-10-11 10:07:43 +0200

    Add order consumer example for bidirectional contract testing

---
+++ OrderConsumer/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderConsumer.Models
+{
+    public class Payment
+    {        
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public int Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ OrderConsumer/PaymentClient.cs
@@ -0,0 +1,28 @@
+﻿namespace OrderConsumer
+{
+    public class PaymentClient
+    {
+        private readonly Uri baseUri;
+
+        public PaymentClient(Uri baseUri)
+        {
+            this.baseUri = baseUri;
+        }
+
+        public async Task<HttpResponseMessage> GetPayment(string id)
+        {
+            using (var client = new HttpClient { BaseAddress = baseUri })
+            {
+                try
+                {
+                    var response = await client.GetAsync($"/payment/{id}");
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("There was a problem connecting to the PaymentProvider API.", ex);
+                }
+            }
+        }
+    }
+}
